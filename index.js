b06cfb51d95f722e66b6f84d5888e03352220720
laurenelizabethmail@yahoo.com <laurenelizabethmail@yahoo.com>
2018-05-13 18:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, yourname) {
-  return 'Happy ${holiday}, ${yourname}!'
+  return `Happy ${holiday}, ${yourname}!`
 }
 function holidayCountdown(days, holiday) {
-  return "It's ${days} days until ${holiday}!"  
+  return `It's ${days} days until ${holiday}!`  
 }
 
